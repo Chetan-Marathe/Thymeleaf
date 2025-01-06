46045786cc6ac62ddb6f276cf1783c1b9b30bd32
Chetan-Marathe <marathecj2004@gmail.com>
2025-01-06 17:27:36 +0530

    Latest commit

--- thymeleaf/src/main/java/com/example/thymeleaf/controller/MainController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class MainController {
 
@@ -13,4 +15,27 @@ public class MainController {
         model.addAttribute("name","Thymeleaf");
         return "home";
     }
+
+    @GetMapping("/elvis")
+    public String elvisexample(Model model){
+        model.addAttribute("isadmin","true");
+        model.addAttribute("gender","F");
+
+
+        return "elvis";
+    }
+
+    @GetMapping("/each")
+    public String eachExample(Model model){
+        //Model is a part of org.springframework.ui
+        List<String> stringList = List.of("First","Second","Third");
+        model.addAttribute("list",stringList);
+        return "each";//refers to the html file
+    }
+
+    /*
+    * Three conditional statements for thymeleaf
+    * Elvis operator
+    * If unless
+    * Switch case*/
 }
