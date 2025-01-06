efa260ecb3f1c1712e95526f95ec938d4eb1a9fe
Chetan-Marathe <marathecj2004@gmail.com>
2025-01-06 12:36:44 +0530

    Latest commit

---
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/home")
+    public String loadHome(Model model){
+        //Model is a part of org.springframework.ui
+        model.addAttribute("name","Thymeleaf");
+        return "home";
+    }
+}
